79b2c87997bfc62f0a8f7cf1c50c1e597ec37157
Pedro <soporte@peruviandigital.com>
2025-01-22 19:40:44 -0500

    use funtion to user file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    @Post('login')
-    login(){
-        return 'login';
+
+    constructor(
+        private readonly authService: AuthService
+    ){}
+
+    @Post('register')
+    register(@Body() newUser: CreateUserDto){
+        if (newUser)
+        return this.authService.register(newUser);
     }
+
+    @Post('login')
+    login(@Body() user: LoginDto){
+        return this.authService.login(user);
+    }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,21 @@
+
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(){}
+    constructor(
+        private readonly usersService: UsersService,
+    ) { }
 
-    async signIn(){}
+    async register(user: CreateUserDto) {
+        return await this.usersService.createUser(user)
+    }
+
+    async login({email, password}: LoginDto) {
+        return await this.usersService.findOneByEmail({email, password})
+    }
 
 }
